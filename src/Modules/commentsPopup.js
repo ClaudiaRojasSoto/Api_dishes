82afdc86002a1bf5581a19f28d0bc55cb460b838
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-06-27 11:18:11 -0400

    Add the tag form to wrap the input and text area tags in commentsPopup.js file

--- src/Modules/commentsPopup.js
+++ src/Modules/commentsPopup.js
@@ -6,14 +6,20 @@ const showCommentsPopup = async (id) => {
   const popup = document.createElement('div');
   popup.id = 'commentsPopup';
   popup.innerHTML = `
-    <h2>${details.strMeal}</h2>
-    <img src="${details.strMealThumb}" alt="${details.strMeal}">
-    <p id="details-instructions">${details.strInstructions}</p>
-    <h3>Add a comment</h3>
-    <input type="text" placeholder="Your name" id="nameInput">
-    <textarea placeholder="Your comment" id="commentInput"></textarea>
-    <button class="closeButton">&times;</button>
-  `;
+  <h2>${details.strMeal}</h2>
+  <img src="${details.strMealThumb}" alt="${details.strMeal}">
+  <p id="details-instructions">${details.strInstructions}</p>
+  <h3>Add a comment</h3>
+  <form>
+    <div>
+      <input type="text" id="nameInput" placeholder="Your name">
+    </div>
+    <div>
+      <textarea id="commentInput" placeholder="Your comment"></textarea>
+    </div>
+  </form>
+  <button class="closeButton">&times;</button>
+`;
 
   document.body.appendChild(popup);
 
