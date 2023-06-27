2926f4d912bf6f2d860465ea3d9b2d4632ee40a7
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-06-26 23:03:58 -0400

    Add to popup.innerHTML the h3 tag for add a comment, the input for the name and a text area for the comment

--- src/Modules/commentsPopup.js
+++ src/Modules/commentsPopup.js
@@ -2,21 +2,25 @@ import getItemDetails from './getItemsDetails.js';
 
 const showCommentsPopup = async (id) => {
   const details = await getItemDetails(id);
-  
+
   const popup = document.createElement('div');
   popup.id = 'commentsPopup';
   popup.innerHTML = `
     <h2>${details.strMeal}</h2>
     <img src="${details.strMealThumb}" alt="${details.strMeal}">
-    <p>${details.strInstructions}</p>
-    <button id="close">Close</button>
+    <p id="details-instructions">${details.strInstructions}</p>
+    <h3>Add a comment</h3>
+    <input type="text" placeholder="Your name" id="nameInput">
+    <textarea placeholder="Your comment" id="commentInput"></textarea>
+    <button class="closeButton">&times;</button>
   `;
-  
+
   document.body.appendChild(popup);
-  
-  document.querySelector('#close').addEventListener('click', () => {
+
+  popup.querySelector('.closeButton').addEventListener('click', () => {
     document.body.removeChild(popup);
   });
 };
 
 export default showCommentsPopup;
+
