1f439fe16b42cf13fbacfa1ab93dd1bab04108ee
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-06-28 00:11:43 -0400

    Fix linters errors and add a comment for do that on the if block to avoid the error in the code and upload the dist folder and the files inside it

--- src/Modules/commentsPopup.js
+++ src/Modules/commentsPopup.js
@@ -1,13 +1,13 @@
-import getItemDetails from "./getItemsDetails.js";
-import getComments from "./getComments.js";
-import sendComment from "./sendComment.js";
+import getItemDetails from './getItemsDetails.js';
+import getComments from './getComments.js';
+import sendComment from './sendComment.js';
 
 const showCommentsPopup = async (id) => {
   const details = await getItemDetails(id);
-  const app_id = "k513WvYOj4wUaRdaeuNF";
+  const appId = 'k513WvYOj4wUaRdaeuNF';
 
-  const popup = document.createElement("div");
-  popup.id = "commentsPopup";
+  const popup = document.createElement('div');
+  popup.id = 'commentsPopup';
   popup.innerHTML = `
   <h2>${details.strMeal}</h2>
   <img src="${details.strMealThumb}" alt="${details.strMeal}">
@@ -28,37 +28,35 @@ const showCommentsPopup = async (id) => {
 
   document.body.appendChild(popup);
 
-  const commentsContainer = popup.querySelector("#commentsContainer");
+  const commentsContainer = popup.querySelector('#commentsContainer');
 
   const updateComments = async () => {
-    const comments = await getComments(app_id, id);
-    if(!Array.isArray(comments)){
+    const comments = await getComments(appId, id);
+    if (!Array.isArray(comments)) {
+      // This block it's designed to handle the empty data in comments
     } else {
-    commentsContainer.innerHTML = '';
-    comments.forEach((comment) => {
-        
-      const commentElement = document.createElement("p");
-      commentElement.textContent = `${comment.creation_date} - ${comment.username}: ${comment.comment}`;
-      commentElement.classList.add("comment-text");
-      commentsContainer.appendChild(commentElement);
-    
-    });
-}
+      commentsContainer.innerHTML = '';
+      comments.forEach((comment) => {
+        const commentElement = document.createElement('p');
+        commentElement.textContent = `${comment.creation_date} - ${comment.username}: ${comment.comment}`;
+        commentElement.classList.add('comment-text');
+        commentsContainer.appendChild(commentElement);
+      });
+    }
   };
 
   await updateComments();
 
-  const form = popup.querySelector("#commentForm");
+  const form = popup.querySelector('#commentForm');
   form.addEventListener('submit', async (event) => {
     event.preventDefault();
-    const username = popup.querySelector("#nameInput").value;
-    const comment = popup.querySelector("#commentInput").value;
-    await sendComment(app_id, id, username, comment);
-    console.log(app_id)
+    const username = popup.querySelector('#nameInput').value;
+    const comment = popup.querySelector('#commentInput').value;
+    await sendComment(appId, id, username, comment);
     await updateComments();
   });
 
-  popup.querySelector(".closeButton").addEventListener("click", () => {
+  popup.querySelector('.closeButton').addEventListener('click', () => {
     document.body.removeChild(popup);
   });
 };
